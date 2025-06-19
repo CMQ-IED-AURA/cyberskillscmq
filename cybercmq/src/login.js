f730a6491f8a1f8e20136300f6eb36a7f97a0fa8
pipetj <jordan.pipetmmi@gmail.com>
2025-06-19 17:21:53 +0200

    update css

--- cybercmq/src/login.js
+++ cybercmq/src/login.js
@@ -1,3 +1,4 @@
+// Login.jsx
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
@@ -37,8 +38,19 @@ function Login() {
         setShowPassword(!showPassword);
     };
 
+    const handleBackClick = () => {
+        navigate(-1); // Retour à la page précédente
+    };
+
     return (
         <div className="page">
+            <button className="back-button" onClick={handleBackClick}>
+                <svg viewBox="0 0 24 24">
+                    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
+                </svg>
+                Retour
+            </button>
+
             <form onSubmit={handleLogin}>
                 <h2>Connexion</h2>
                 <div className="input-container">
--- cybercmq/src/register.js
+++ cybercmq/src/register.js
@@ -37,8 +37,19 @@ function Register() {
         setShowPassword(!showPassword);
     };
 
+    const handleBackClick = () => {
+        navigate(-1); // Retour à la page précédente
+    };
+
     return (
         <div className="page">
+            <button className="back-button" onClick={handleBackClick}>
+                <svg viewBox="0 0 24 24">
+                    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
+                </svg>
+                Retour
+            </button>
+
             <form onSubmit={handleRegister}>
                 <h2>Inscription</h2>
                 <div className="input-container">
